6c34c80c20352fcffd702017e5a54f19954f2487
Jethro Williams <jethrowilliams@outlook.com>
2021-12-01 10:45:08 +0000

    Fix a typo in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origin "http://localhost:3000"
+    origins "http://localhost:3000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
-    origin "https://my-front-end-app-url.com" # Update this accordingly to the URL of your front-end
+    origins "https://my-front-end-app-url.com" # Update this accordingly to the URL of your front-end
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
