23372589594ae30394320423a4280a118f996ff1
Jethro Williams <jethrowilliams@outlook.com>
2021-12-01 12:18:17 +0000

    Update the session store to work in all environments
    Previously it would have worked in production, but not in development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "my-backend-app-url.com" # Update this to whatever your URL is
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "my-backend-app-url.com" # Update this to whatever your URL is
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
